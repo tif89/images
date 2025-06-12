5f98000927c7b7367fb661db6451f7354fab262f
tif89 <contact.tifgames@gmail.com>
2025-06-12 11:48:28 +0200

    Ajout des images PNG

---
+++ config.lua
@@ -0,0 +1,27 @@
+Config = {
+    serverName = 'PERP',
+
+    Keyboard = 'F9',
+
+    ESXUse = true,
+    itemName = 'tph700',
+    itemUse = true,
+
+    propName = 'prop_cs_hand_radio',
+
+    Effect = true,
+    Animation = true,
+    PushtoTalk = 'LMENU',
+
+    Restricted = {},
+    Names = {
+        [17] = 'Police Nationale',
+        [18] = 'Sapeurs-Pompiers'
+    },
+}
+
+Config.Channels = {
+    { frequency = 17, label = 'Police Nationale' },
+    { frequency = 18, label = 'Sapeurs-Pompiers' },
+    { frequency = 19, label = 'Gendarmerie' }
+}
